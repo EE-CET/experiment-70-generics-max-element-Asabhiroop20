d6f68259b590ea9adfbb455226131ca3d460605b
Asabhiroop20 <abhiroopas2423@gmail.com>
2026-04-10 00:54:23 +0530

    First commit of Max element program

--- src/Solution.java
+++ src/Solution.java
@@ -2,20 +2,40 @@ import java.util.Scanner;
 
 public class Solution {
     
-    // TODO: Write the generic method findMax(T[] array) here
-    // Hint: The type parameter T must extend Comparable<T>
-    
+    // Generic method to find max
+    public static <T extends Comparable<T>> T findMax(T[] array) {
+        T max = array[0];
+        
+        for (int i = 1; i < array.length; i++) {
+            if (array[i].compareTo(max) > 0) {
+                max = array[i];
+            }
+        }
+        
+        return max;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        
-        // TODO: Call findMax on intArray and print the result
-        // Format: "Max Integer: [val]"
-        
-        // TODO: Call findMax on strArray and print the result
-        // Format: "Max String: [val]"
-        
+
+        // Integer array
+        int n1 = scanner.nextInt();
+        Integer[] intArray = new Integer[n1];
+        for (int i = 0; i < n1; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        // String array
+        int n2 = scanner.nextInt();
+        String[] strArray = new String[n2];
+        for (int i = 0; i < n2; i++) {
+            strArray[i] = scanner.next();
+        }
+
+        // Output
+        System.out.println("Max Integer: " + findMax(intArray));
+        System.out.print("Max String: " + findMax(strArray));
+
         scanner.close();
     }
 }
